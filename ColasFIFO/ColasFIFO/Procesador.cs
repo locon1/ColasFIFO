c677a473f6b506ba940741c2423e1bedd2f1f98f
José Ocon <locon1@ucol.mx>
2017-06-14 06:15:56 -0500

    Add files via upload

---
+++ ColasFIFO/ColasFIFO/Procesador.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColasFIFO
+{
+    class Procesador
+    {
+        public Proceso inicio { get; private set; }
+        public Proceso fin { get; private set; }
+        public int _totalProcesos { get; private set; }
+        public int _ciclosNecesarios { get; private set; }
+
+        public Procesador()
+        {
+            this._totalProcesos = 0;
+            this._ciclosNecesarios = 0;
+            this.inicio = null;
+            this.fin = null;
+        }
+
+        public void push(Proceso nuevo)
+        {
+            if (inicio != null) 
+            {
+                fin.siguente = nuevo;
+                fin = nuevo;
+            }
+            else 
+            {
+                inicio = nuevo;
+                fin = nuevo;
+            }
+            _totalProcesos++;
+            _ciclosNecesarios += nuevo._ciclosProceso;
+        }
+        
+        public void pop(int ciclosR)
+        {
+            if (ciclosR == 0) 
+            {
+                inicio = inicio.siguente;
+                _totalProcesos--;
+            }
+        }
+
+        public int procesar()
+        {
+            int restantes;
+            restantes = inicio.ciclosRestantes();
+            _ciclosNecesarios--;
+            return restantes;
+        }
+
+    }
+}
---
+++ ColasFIFO/ColasFIFO/Proceso.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColasFIFO
+{
+    class Proceso
+    {
+        public Proceso siguente { get; set; }
+        static Random random = new Random();
+        public int _ciclosProceso { get; private set; }
+
+        public Proceso()
+        {
+            _ciclosProceso = random.Next(4, 15);
+            siguente = null;
+        }
+
+        public int ciclosRestantes()
+        {
+            _ciclosProceso--;
+            return _ciclosProceso;
+        }
+
+        public override string ToString()
+        {
+            return "Ciclos necesarios: " + _ciclosProceso + Environment.NewLine;
+        }
+    }
+}
